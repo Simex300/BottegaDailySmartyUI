32b3339e8b220339c45f28b3ad4232a30de11967
PC\Adonis <adonis0444@gmail.com>
2019-03-11 10:29:28 -0400

    Adding topic to the post component

--- src/components/post.js
+++ src/components/post.js
@@ -2,10 +2,17 @@ import React, { Component } from 'react';
 
 class Post extends Component {
     render() {
-        const {title, content} = this.props;
+        const {title, associated_topics} = this.props;
         return(
             <div className='post'>
-                <h2>{title}</h2>
+                <div className='post__title'>
+                    {title}
+                </div>
+                <div className='post__topics'>
+                    {associated_topics.map((topic, index) => {
+                        return <span key={index} className='post__topics-item'>{topic} </span>
+                    })}
+                </div>
             </div>
         )
     }
